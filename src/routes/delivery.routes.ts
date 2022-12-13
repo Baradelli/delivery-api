e77024ca967401bbf92571cd4644c883e7867c73
Vitor Baradelli <vitorbaradelli@gmail.com>
2022-12-12 21:48:24 -0300

    middlewares and getDeliveries

--- src/routes/delivery.routes.ts
+++ src/routes/delivery.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express'
+import { ensureAuthenticateClient } from '../middlewares/ensureAuthenticateClient'
+import { ensureAuthenticateDeliveryman } from '../middlewares/ensureAuthenticateDeliveryman'
 import { CreateDeliveryController } from '../modules/deliveries/useCases/createDelivery/CreateDeliveryController'
+import { FindAllAvailableController } from '../modules/deliveries/useCases/findAllAvailable/findAllAvailableController'
 
 const routes = Router()
 
 const createDeliveryController = new CreateDeliveryController()
+const findAllAvailableController = new FindAllAvailableController()
 
-routes.post('/delivery', createDeliveryController.handle)
+routes.post(
+  '/delivery',
+  ensureAuthenticateClient,
+  createDeliveryController.handle
+)
+
+routes.get(
+  '/delivery/available',
+  ensureAuthenticateDeliveryman,
+  findAllAvailableController.handle
+)
 
 export { routes }
